74482734bdd956ea2b7bfcf636bc9f68689e2bd8
Jorge Robles Baneres <jorge.robles.baneres@gmail.com>
2022-01-06 14:42:05 -0500

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -22,15 +22,18 @@ var fight = function(enemyName) {
             if (confirmSkip) {
                 window.alert(playerName + " has decided to skip this fight. Goodbye!");
                 // subtract money from playerMoney for skipping
-                playerMoney = playerMoney - 10;
+                playerMoney = Math.max(0, playerMoney - 10);
                 console.log("playerMoney", playerMoney)
                 break;
             }
         }
         // if player choses to fight, then fight
         else if (promptFight === "fight" || promptFight === "FIGHT") {
-            // remove enemy's health by subtracting the amount set in the playerAttack variable
-            enemyHealth = enemyHealth - playerAttack;
+           
+            // generate random damage value based on player's attack power
+            var damage = randomNumber(playerAttack - 3, playerAttack);
+
+            enemyHealth = Math.max(0, enemyHealth - playerAttack);
             console.log(
                 playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
             );
@@ -49,7 +52,7 @@ var fight = function(enemyName) {
             }
 
             // remove player's health by subtracting the amount set in the enemyAttack variable
-            playerHealth = playerHealth - enemyAttack;
+            playerHealth = Math.max(0, playerHealth - enemyAttack);
             console.log(
                 enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
             );
@@ -86,8 +89,7 @@ var startGame = function() {
         
             var pickedEnemyName = enemyNames[i];
 
-            enemyHealth = 50;
-
+            enemyHealth = randomNumber(40,60);
             fight(pickedEnemyName);
 
             //if we're not at the last enemy in the array
@@ -186,6 +188,12 @@ var shop = function() {
     }
 }
 
+//function to generate a random numeric value
+var randomNumber = function(min, max) {
+    var value = Math.floor(Math.random() * (max - min + 1) + min);
+
+    return value;
+}
 
 //start the game when the page loads
 startGame();
