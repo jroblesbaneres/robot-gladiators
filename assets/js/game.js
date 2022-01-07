e8c5f990972835849e048e6847ad06be310d3c39
Jorge Robles Baneres <jorge.robles.baneres@gmail.com>
2022-01-06 23:19:16 -0500

    blank

--- assets/js/game.js
+++ assets/js/game.js
@@ -166,8 +166,21 @@ var randomNumber = function(min, max) {
     return value;
 }
 
+//function to set name
+var getPlayerName = function() {
+    var name = "";
+
+    // ADD LOOP HERE
+    while (name === "" || name === null) {
+        name = prompt("What is your robot's name?");
+    }
+
+    console.log("Your robot's name is " + name);
+    return name;
+};
+
 var playerInfo = {
-    name: window.prompt("What is your robot's name?"),
+    name: getPlayerName(),
     health: 100,
     attack: 10,
     money: 10,
